48a04c0ef8a0337a0c4cf736c41b14b454091347
Jose Gerardo Gomez Lopez <jglopez1@latam.stefanini.com>
2024-11-21 18:35:46 -0500

    Add first commmit

---
+++ 01-spring-boot-overview/06-properties-demo/mycoolapp/src/main/java/com/luv2code/demo/mycoolapp/rest/MyCoolAppController.java
@@ -0,0 +1,57 @@
+package com.luv2code.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyCoolAppController {
+
+//    Inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+//    Expose a new endpoint for "teaminfo"
+    @GetMapping("/team-info")
+    public String getTeamInfo(){
+        return "Team name: " + teamName + ", Coach name: " + coachName;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello world";
+    }
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
+    }
+}
